87d595c9e5620bf60055e33c6de78222195c486a
Ana Cristina Silva Solis <cristinasilvasolis09@gmail.com>
2024-11-24 14:07:24 -0500

    estructura proyecto

---
+++ src/main/java/com/conce_carros/ConcesionarioCarrosBackApplication.java
@@ -0,0 +1,13 @@
+package com.conce_carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesionarioCarrosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesionarioCarrosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conce_carros/ConcesionarioCarrosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conce_carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcesionarioCarrosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
